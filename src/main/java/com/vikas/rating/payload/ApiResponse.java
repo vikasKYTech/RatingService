74f1d628ed2e94b6fb8e61bb7753e79d9aeac0f9
vikas <vikasraj.yad@gmail.com>
2025-12-28 00:26:49 +0530

    2nd commit

---
+++ src/main/java/com/vikas/rating/payload/ApiResponse.java
@@ -0,0 +1,16 @@
+package com.vikas.rating.payload;
+
+import lombok.*;
+import org.springframework.http.HttpStatus;
+
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse {
+    private String message;
+    private boolean success;
+    private HttpStatus status;
+}
